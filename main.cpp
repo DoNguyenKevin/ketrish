7fd55fa7b686ee447fbb6f6a685f6764cfd48cfd
Tạ Quang Khôi <khoi0941@gmail.com>
2024-10-23 23:56:11 +0700

    Save code from ChatGPT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <cairo.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cairo_surface_t *surface;
+    cairo_t *cr;
+
+    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 240, 80);
+    cr = cairo_create(surface);
+
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_paint(cr);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size(cr, 40);
+    cairo_move_to(cr, 10, 50);
+    cairo_show_text(cr, "Hello, Cairo!");
+
+    cairo_destroy(cr);
+    cairo_surface_write_to_png(surface, "output.png");
+    cairo_surface_destroy(surface);
+
     return 0;
 }
